608dc49ca09dcb08505ebc3e7d241f978ef5a654
Kanru Hua <k.hua.kanru@ieee.org>
2016-02-12 17:20:57 +0000

    Add 32-bit support

--- src/gen/genrev.c
+++ src/gen/genrev.c
@@ -7,7 +7,7 @@ NAME
     genrev - generates bit-reverse C codes for fft implementation.
 
 SYNOPSIS
-    genrev [-a] [-b] [-n name] fftpower
+    genrev [-a] [-b] [-x] [-n name] fftpower
 
 OPTIONS
     -a
@@ -16,6 +16,9 @@ OPTIONS
     -b
         Leave one blank line in the end.
     
+    -x
+        Generate assembly code for x86_64 architecture. (default is x86)
+    
     -n
         Specify the name of the function. In the generated code, an underline
         and fftpower will be added after this name. e.g. "name_4".
@@ -23,7 +26,7 @@ OPTIONS
         The default name is bitrev.
 */
 
-void bitrev_genstatic_asm(int power, char* name)
+void bitrev_genstatic_asm(int power, char* name, int x86_64)
 {
     int N, i, j;
     int tmp = 0x00000000;
@@ -33,10 +36,18 @@ void bitrev_genstatic_asm(int power, char* name)
     printf("{\n");
     printf("    __asm__ __volatile__\n");
     printf("    (\n");
-    printf("        \"mov %%0, %%%%rsi\\n\"\n");
-    printf("        \"mov %%1, %%%%rdi\\n\"\n");
-    printf("        \"movss (%%%%rdi), %%%%xmm0\\n\"\n");
-    printf("        \"movss %%%%xmm0, (%%%%rsi)\\n\"\n");
+    if(x86_64)
+    {
+        printf("        \"mov %%0, %%%%rax\\n\"\n");
+        printf("        \"mov %%1, %%%%rdx\\n\"\n");
+        printf("        \"movss (%%%%rdx), %%%%xmm0\\n\"\n");
+        printf("        \"movss %%%%xmm0, (%%%%rax)\\n\"\n");
+    } else {
+        printf("        \"mov %%0, %%%%eax\\n\"\n");
+        printf("        \"mov %%1, %%%%edx\\n\"\n");
+        printf("        \"movss (%%%%edx), %%%%xmm0\\n\"\n");
+        printf("        \"movss %%%%xmm0, (%%%%eax)\\n\"\n");
+    }
     
     for(i = 0; i < N - 1; i ++)
     {
@@ -48,13 +59,25 @@ void bitrev_genstatic_asm(int power, char* name)
         }
         tmp |= (1 << j);
         
-        printf("        \"movss %d(%%%%rdi), %%%%xmm%d\\n\"\n", tmp * 4, i % 8);
-        printf("        \"movss %%%%xmm%d, %d(%%%%rsi)\\n\"\n", i % 8, i * 4 + 4);
+        if(x86_64)
+        {
+            printf("        \"movss %d(%%%%rdx), %%%%xmm%d\\n\"\n", tmp * 4, i % 8);
+            printf("        \"movss %%%%xmm%d, %d(%%%%rax)\\n\"\n", i % 8, i * 4 + 4);
+        } else {
+            printf("        \"movss %d(%%%%edx), %%%%xmm%d\\n\"\n", tmp * 4, i % 8);
+            printf("        \"movss %%%%xmm%d, %d(%%%%eax)\\n\"\n", i % 8, i * 4 + 4);
+        }
     }
     printf("    : \"+m\"(dst)\n");
     printf("    : \"m\"(src)\n");
-    printf("    : \"%%xmm0\", \"%%xmm1\", \"%%xmm2\", \"%%xmm3\", \"%%xmm4\", "
-                 "\"%%xmm5\", \"%%xmm6\", \"%%xmm7\", \"%%rsi\", \"%%rdi\"\n");
+    if(x86_64)
+    {
+        printf("    : \"%%xmm0\", \"%%xmm1\", \"%%xmm2\", \"%%xmm3\", \"%%xmm4\", "
+                     "\"%%xmm5\", \"%%xmm6\", \"%%xmm7\", \"%%rax\", \"%%rdx\"\n");
+    } else {
+        printf("    : \"%%xmm0\", \"%%xmm1\", \"%%xmm2\", \"%%xmm3\", \"%%xmm4\", "
+                     "\"%%xmm5\", \"%%xmm6\", \"%%xmm7\", \"%%eax\", \"%%edx\"\n");
+    }
     printf("    );\n");
     printf("}\n");
 }
@@ -90,10 +113,11 @@ int main(int argc, char** argv)
     int c;
     int aflag = 0;
     int bflag = 0;
+    int xflag = 0;
     int power = 0;
     char* name = "bitrev";
     
-    while((c = getopt(argc, argv, "abn:")) != -1)
+    while((c = getopt(argc, argv, "abxn:")) != -1)
     {
         switch(c)
         {
@@ -103,11 +127,14 @@ int main(int argc, char** argv)
             case 'b':
                 bflag = 1;
             break;
+            case 'x':
+                xflag = 1;
+            break;
             case 'n':
                 name = optarg;
             break;
             default:
-                fprintf(stderr, "Usage: genrev [-a] [-b] fftpower\n");
+                fprintf(stderr, "Usage: genrev [-a] [-b] [-x] [-n name] fftpower\n");
             return 1;
         }
     }
@@ -118,13 +145,13 @@ int main(int argc, char** argv)
     if(optind >= argc)
     {
         fprintf(stderr, "Missing argument 'fftpower'.\n");
-        fprintf(stderr, "Usage: genrev [-a] [-b] fftpower\n");
+        fprintf(stderr, "Usage: genrev [-a] [-b] [-x] [-n name] fftpower\n");
         return 1;
     }
     
     power = atoi(argv[optind]);
     if(aflag)
-        bitrev_genstatic_asm(power, name);
+        bitrev_genstatic_asm(power, name, xflag);
     else
         bitrev_genstatic(power, name);
     
